faefce455c0353a6b5ed188b39d486b4e44d00f3
Rullow <rullowgl@gmail.com>
2021-03-01 22:20:51 +0100

    Removed gen_test()

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import solution
-import random
-
-
-def gen_test():
-    M = random.randint(1, 8)
-    F = random.randint(1, 8)
-    print ("Objective: " + str(M) + "M " + str(F) + "F")
-    return M, F
 
 
 def main():
